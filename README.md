98839ea51cd3390037fdd6fd82e16183ce12cd8f
dimilidi <dimitrovalidia@yahoo.com>
2023-07-12 13:00:58 +0200

    add header

---
+++ README.md
@@ -0,0 +1,9 @@
+## Workflow
+1. Create Angular App
+- create projects folder
+- create app as frontend
+2. Add Header
+- generate component (ng g c header)
+- install angular material (ng add @angular/material)
+- add HTML
+- add CSS
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/partials/header/header.component';
+
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, 
+    AppRoutingModule, 
+    MatIconModule, BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
