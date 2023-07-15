2bffa3f9a0f67d149b205dd4a3d963ec52e99331
dimilidi <dimitrovalidia@yahoo.com>
2023-07-15 12:59:54 +0200

    .

--- backend/built/server.js
+++ backend/built/server.js
@@ -11,6 +11,7 @@ var productRouter_1 = __importDefault(require("./routers/productRouter"));
 var userRouter_1 = __importDefault(require("./routers/userRouter"));
 var orderRouter_1 = __importDefault(require("./routers/orderRouter"));
 var database_config_1 = require("./configs/database_config");
+var path_1 = __importDefault(require("path"));
 // DB CONNECT
 (0, database_config_1.dbConnect)();
 // CREATE EXPRESS SERVER 
@@ -21,11 +22,16 @@ app.use((0, cors_1.default)({
     credentials: true,
     origin: ["http://localhost:4200"]
 }));
-app.use(express_1.default.static('public'));
+//app.use('/', express.static('/build'))
 // ROUTERS
 app.use('/products', productRouter_1.default);
 app.use('/users', userRouter_1.default);
 app.use('/orders', orderRouter_1.default);
+app.use(express_1.default.static('public'));
+//catch-all route handler 
+app.get('*', function (req, res) {
+    res.sendFile(path_1.default.join(__dirname, 'public', 'index.html'));
+});
 var port = process.env.PORT || 5000;
 app.listen(port, function () {
     console.log("Surver running on http://localhost:" + port);
