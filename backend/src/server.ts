a39f420e240aeae7b5720358682c8011abbff7e1
dimilidi <dimitrovalidia@yahoo.com>
2023-07-28 02:57:47 +0200

    build

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -19,7 +19,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     credentials:true,
-    origin:["http://localhost:4200"]
+    origin:["https://web-store-85da.onrender.com"]
 }));
 //app.use('/', express.static('/build'))
 
